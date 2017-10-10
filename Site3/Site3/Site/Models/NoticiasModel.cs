be14c443c6d2d8b0950be80bd7707c3b6e5af0fb
grupoamlrt <grupoamlrt@gmail.com>
2017-10-09 22:41:53 -0300

    Att

--- Site3/Site3/Site/Models/NoticiasModel.cs
+++ Site3/Site3/Site/Models/NoticiasModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +9,9 @@ namespace Site.Models
 {
     public class NoticiasModel
     {
+        [Key]
         public int Codigo { get; set; }
+        //[ForeignKey]
         public string Titulo { get; set; }
         public string Descricao { get; set; }
     }
